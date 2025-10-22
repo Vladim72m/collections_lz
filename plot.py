7d8efb13333788df5287e0b6cd2a9bfb1cc59fc1
Vladim72m <vladim72dx@tutamail.com>
2025-10-22 21:40:17 +0300

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 30, 45) # задаем диапазон значений x
+y = x**3 + 1# вычисляем значения y
+plt.plot(x, y) # строим график
+plt.show() # отображаем график

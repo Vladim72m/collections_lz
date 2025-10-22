6d7319d337790fc289839d6f089817f61e5ce32e
Vladim72m <vladim72dx@tutamail.com>
2025-10-22 15:48:06 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,19 @@
+N = int(input())
+a = 0
+b = 1
+i = 2
+row = [a, b]
+while i < N: 
+    a, b = b, a + b
+    row.append(b) 
+    i += 1
+print(row) 
+for i in range(N): 
+    if row[i] % 2 == 0: 
+        row[i] = row[i] * 2 
+    else: 
+        row[i] = row[i] ** 2
+print(row) 
+    row_max = max(row) 
+    print(max(row)) 
+    

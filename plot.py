456e41820aa1816061679173d719d509a0dc60f9
Vladim72m <vladim72dx@tutamail.com>
2025-10-22 21:45:19 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 x = np.linspace(0, 30, 45) # задаем диапазон значений x
-y = x**3 + 1# вычисляем значения y
+y = x**3 + 1 # вычисляем значения y
 plt.plot(x, y) # строим график
+
 plt.show() # отображаем график
